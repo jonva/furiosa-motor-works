73110ed56597b7d397903216a4a043ed0595a9c4
Nikhil John <nikhiljo@juniper.net>
2016-02-05 18:31:31 +0530

    Added responsiveness. Styled

--- public/scripts/reviews.js
+++ public/scripts/reviews.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
-			$('#review-table').DataTable({
-				"paging":   false,
-				"info": false,
-				"searching": true,
-				
-			});
-		});
+	$('#review-table').DataTable({
+		"paging":   false,
+		"info": false,
+		"searching": true,
+		"oLanguage": {
+			"sSearch": ""
+		},
+		initComplete: function(){
+			$('.dataTables_filter > label > input').attr({'placeholder': 'Search'});
+		}
+	});
+});
--- public/scripts/services.js
+++ public/scripts/services.js
@@ -1,54 +1,30 @@
-// $(document).ready(function() {
-// 			$('#services-table').DataTable({
-// 				"paging":   false,
-// 			    "info": false,
-// 			    "searching": false,
-// 			    initComplete: function () {
-// 		            this.api().columns().every( function () {
-// 		                var column = this;
-// 		                var select = $('<select><option value=""></option></select>')
-// 		                    .appendTo( $(column.footer()).empty() )
-// 		                    .on( 'change', function () {
-// 		                        var val = $.fn.dataTable.util.escapeRegex(
-// 		                            $(this).val()
-// 		                        );
-		 
-// 		                        column
-// 		                            .search( val ? '^'+val+'$' : '', true, false )
-// 		                            .draw();
-// 		                    } );
-		 
-// 		                column.data().unique().sort().each( function ( d, j ) {
-// 		                    select.append( '<option value="'+d+'">'+d+'</option>' )
-// 		                } );
-// 		            } );
-// 		        }
-// 			});
-// 		});
-
 $(document).ready(function() {
-    $('#services-table').DataTable( {
-    	"paging":   false,
-		"info": false,
-        initComplete: function () {
-            ( function () {
+    $('#services-table').DataTable({
+        "paging": false,
+        "info": false,
+        "oLanguage": {
+            "sSearch": ""
+        },
+        initComplete: function() {
+        	$('.dataTables_filter > label > input').attr({'placeholder': 'Search'});
+            (function() {
                 var column = this;
                 var select = $('<select><option value=""></option></select>')
-                    .appendTo( $(column.footer()).empty() )
-                    .on( 'change', function () {
+                    .appendTo($(column.footer()).empty())
+                    .on('change', function() {
                         var val = $.fn.dataTable.util.escapeRegex(
                             $(this).val()
                         );
- 
+
                         column
-                            .search( val ? '^'+val+'$' : '', true, false )
+                            .search(val ? '^' + val + '$' : '', true, false)
                             .draw();
-                    } );
- 
-                $.each($.unique(column.data()[0]), function ( d, j ) {
-                    select.append( '<option value="'+j+'">'+j+'</option>' )
-                } );
-            } ).call(this.api().columns(2));
+                    });
+
+                $.each($.unique(column.data()[0]), function(d, j) {
+                    select.append('<option value="' + j + '">' + j + '</option>')
+                });
+            }).call(this.api().columns(2));
         }
-    } );
-} );
+    });
+});
